4e9e1bea3fd95de82f3c6500d75c6efb70b3502a
Olamilekan Omosanya <olamilekanomosanya@gmail.com>
2022-06-20 11:21:22 +0100

    add sibl and lbic

---
+++ app/Models/User.php
@@ -0,0 +1,91 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+     protected $fillable = [
+        'owners_name',
+        'occupiers_name',
+        'phone',
+        'location',
+        'block_number',
+        'flat_number',
+        'img',
+        'email',
+        'password',
+    ];
+
+
+
+     public function lbic()
+    {
+        return $this->usertype == 1 ? true : false;
+    }
+
+
+    public function sbil()
+    {
+        return $this->usertype == 2 ? true : false;
+    }
+
+    public function user()
+    {
+        return $this->usertype == 0 ? true : false;
+    }
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }    
+}
